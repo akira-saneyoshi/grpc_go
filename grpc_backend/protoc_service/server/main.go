f74add49f4f7b3ab003aac19c7df9d4d32b60ed9
saneyoshi <aaa0410as@gmail.com>
2024-08-14 21:58:37 +0900

    add:Unary RPCサーバーの実装

---
+++ grpc_backend/protoc_service/server/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"udemy_study/go_grpc/pb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedFileServiceServer
+}
+
+func (*server) ListFiles(ctx context.Context, req *pb.ListFilesRequest) (*pb.ListFilesResponse, error) {
+	fmt.Println("ListFiles was invoked")
+
+	dir := "/opt/protoc_service/storage"
+
+	paths, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var filenames []string
+	for _, path := range paths {
+		if !path.IsDir() {
+			filenames = append(filenames, path.Name())
+		}
+	}
+
+	res := &pb.ListFilesResponse{
+		Filenames: filenames,
+	}
+	return res, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:8090")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterFileServiceServer(s, &server{})
+
+	fmt.Println("server is running...")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
